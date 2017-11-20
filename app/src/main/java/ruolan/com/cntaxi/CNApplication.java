de46524e2a235f27e4329e19deaae96297663ada
wuyinlei <wuyinlei19931118@hotmail.com>
2017-11-20 22:13:07 +0800

    完成简单启动界面逻辑

---
+++ app/src/main/java/ruolan/com/cntaxi/CNApplication.java
@@ -0,0 +1,24 @@
+package ruolan.com.cntaxi;
+
+import android.app.Application;
+
+/**
+ * Created by wuyinlei on 2017/11/20.
+ */
+
+public class CNApplication extends Application {
+
+
+    private static CNApplication INSTANCE;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        INSTANCE = this;
+    }
+
+    //获取一个实例
+    public static CNApplication getInstance() {
+        return INSTANCE;
+    }
+}
